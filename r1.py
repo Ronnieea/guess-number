78c0d05f9169c216c332c1022e462cf9d53986cd
Ronnie Hung <nina.9212301230@gmail.com>
2022-05-28 10:03:48 +0800

    correct commit

--- r1.py
+++ r1.py
@@ -1,12 +1,12 @@
 import random
 r = random.randint(1, 100)
 while True:
-	num = input('請猜數字:')
-	if r == num:
-		input('終於猜對了!')
-		break
-	else:
-		if r > num:
-			input('比答案小')
-		else:
-			input('比答案大')
+    num = input('請猜數字:')
+    num = int(num)
+    if num == r:
+        print('你猜中了!')
+        break
+    elif num > r:
+        print('比答案大')
+    elif num < r:
+        print('比答案小')
